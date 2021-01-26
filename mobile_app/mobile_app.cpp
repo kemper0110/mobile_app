a7b2e7b9d6a8742930743cfe3b15d54acaa476a7
QTNJAQQ <danil@DESKTOP-QTNJAQQ>
2021-01-27 01:28:27 +0300

    butt

--- mobile_app/mobile_app.cpp
+++ mobile_app/mobile_app.cpp
@@ -12,30 +12,46 @@ sf::Font font;
 class Lable {
 protected:
 	sf::Text TXT;
-	Lable() {}
-
 public:
-
-	Lable(std::string text, sf::Vector2f pos, int size = 30) {
+	Lable() {
 		TXT.setFont(font);
-		TXT.setCharacterSize(size);
 		TXT.setFillColor(sf::Color::Black);
 		TXT.setOutlineColor(sf::Color::Blue);
 		TXT.setOutlineThickness(1.f);
+	}
+	Lable(std::string text, sf::Vector2f pos, int size = 30) : Lable(){
+		TXT.setCharacterSize(size);
 		TXT.setPosition(pos);
 		TXT.setString(text);
 	}
-	sf::Text operator()(){
+	sf::Text operator+(){
 		return TXT;
 	}
 };
 
 class Button : Lable{
 	Lable lable;
+	sf::RectangleShape BOX;
 public:
 	Button(sf::Vector2f pos, sf::Vector2f size, std::string text = "") {
 		lable = Lable(text, pos);
 
+		BOX.setFillColor(sf::Color(240, 240, 240));
+		BOX.setOutlineColor(sf::Color::Black);
+		BOX.setPosition(pos);
+		BOX.setSize(size);
+	}
+	bool isSelected(sf::Vector2i mouse) {
+		if (mouse.x > BOX.getPosition().x
+			&& mouse.x < BOX.getSize().x + BOX.getPosition().x
+			&& mouse.y > BOX.getPosition().y
+			&& mouse.y < BOX.getSize().y + BOX.getPosition().y)
+			return true;
+		return false;
+	}
+	void draw(sf::RenderWindow& window) {
+		window.draw(BOX);
+		window.draw(+lable);
 	}
 };
 
@@ -48,16 +64,17 @@ sf::CircleShape make_circle() {
 	c.setPosition(0, 0);
 	return c;
 }
-
+bool lol = true;
 int main(){
 	font.loadFromFile("Century Gothic Regular.ttf");
 
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 4;
 
-	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "mobile", sf::Style::Fullscreen, settings);
+	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "mobile"/*, sf::Style::Fullscreen, settings*/);
 
 	Lable ip_lable("IP input", { (float)window.getSize().x / 2, (float)window.getSize().y / 2 }, 40);
+	Button button({ 10, 10 }, { 100, 100 }, "text");
 	auto circle = make_circle();
 
 	while (1) {
@@ -74,6 +91,13 @@ int main(){
 					return 0;
 				}
 			}
+			if (event.type == sf::Event::MouseButtonPressed) {
+				if (event.MouseButtonPressed == sf::Mouse::Left) {
+					sf::Vector2i pos = sf::Mouse::getPosition();
+				//	if(button.isSelected(pos))
+						lol = !lol;
+				}
+			}
 #endif // windows
 			if (
 #ifdef windows
@@ -87,8 +111,12 @@ int main(){
 			}
 	
 		}
-		window.clear(sf::Color(255, 255, 255));
-		window.draw(ip_lable());
+		if (lol == true)
+			window.clear(sf::Color(255, 255, 255));
+		else
+			window.clear(sf::Color(0, 0, 0));
+		button.draw(window);
+		window.draw(+ip_lable);
 		window.draw(circle);
 		window.display();
 		sf::sleep(sf::milliseconds(10));
